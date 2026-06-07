0a5c36b493dedf1376fac1aedc67cd5afc181c36
Bruno Giovanola <bruno.giovanola@mi.unc.edu.ar>
2026-06-07 00:22:53 -0300

    Agregar StatusController y endpoint ping
    - Se elimino WeatherForecast
    - Se elimino WeatherForecastController
    - Modifico StatusController
    - Implementado GET /api/status/ping
    - Se elimino soporte Docker

---
+++ CoreInventory/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreInventory.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StatusController : ControllerBase
+{
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("pong");
+    }
+}
